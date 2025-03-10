a03fba08f2323a8cd8104cc7c4db8733dd72b0a3
m-zeprzalka <m@zeprzalka.pl>
2025-03-10 19:46:11 +0100

    Dodano naprawiony cms

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -35,12 +35,8 @@ export async function generateMetadata({
   };
 }
 
-export default async function BlogPostPage({
-  params,
-}: {
-  params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
+// Zmieniona definicja funkcji - uproszczone typowanie
+export default async function BlogPostPage({ params }: any) {
   try {
     const post = await getPostBySlug(params.slug);
 
@@ -201,7 +197,6 @@ export default async function BlogPostPage({
                         <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                       </svg>
                     </a>
-
                     <a
                       href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(`https://zeprzalka.pl/blog/${post.slug}`)}&text=${encodeURIComponent(post.title)}`}
                       target="_blank"
@@ -219,7 +214,6 @@ export default async function BlogPostPage({
                         <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
                       </svg>
                     </a>
-
                     <a
                       href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(`https://zeprzalka.pl/blog/${post.slug}`)}`}
                       target="_blank"
